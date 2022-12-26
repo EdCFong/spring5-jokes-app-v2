6bfba052c7e78ead56454eedd06f27dbd8cecee3
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2022-12-26 10:22:54 -0600

    Added service and model

---
+++ src/main/java/models/Quote.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Quote {
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
